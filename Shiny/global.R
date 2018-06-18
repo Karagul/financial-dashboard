c5426378733c017c6d5686e595e16bea5a30a800
Zach O'Brien <40214866+Zacharyobrien@users.noreply.github.com>
2018-06-18 08:46:53 -0700

    Update global.R

--- Shiny/global.R
+++ Shiny/global.R
@@ -1,3 +1,10 @@
 library(shiny)
+source("Server.R")
+source("UI.R")
+source("global.R")
+
 data <- readRDS("healthexp.Rds")
 data$Region <- as.factor(data$Region)
+
+# Run the app ----
+shinyApp(ui = ui, server = server)
