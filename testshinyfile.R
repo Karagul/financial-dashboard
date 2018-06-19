40694dbdc938d6bc80c6ef8e8a25a8dadf95084a
zacharyobrien <zach@invisible.email>
2018-06-18 22:50:49 -0700

    Just a test to see if I can commit to new repo location.

---
+++ testshinyfile.R
@@ -0,0 +1,25 @@
+library(shinydashboard)
+# Global variables can go here
+n <- 200
+
+
+# Define the UI
+ui <- fluidPage(
+    bootstrapPage(
+      numericInput('n', 'Number of obs', n),
+      plotOutput('plot')
+))
+
+
+# Define the server code
+server <- function(input, output) {
+  output$plot <- renderPlot({
+    hist(runif(input$n))
+  })
+}
+
+# Return a Shiny app object
+shinyApp(ui = ui, server = server)
+
+
+ 
