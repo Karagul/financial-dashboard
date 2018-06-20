7ac741d8c6274fa3ff1d362996e95e4b754c0762
liamob <liamob@umich.edu>
2018-06-20 12:09:56 -0700

    how to access google sheets from R

--- Shiny/sheets_data.R
+++ Shiny/sheets_data.R
@@ -1,9 +1,7 @@
-library(googlesheets)
+gs_auth
+gs_ls() # not neccessary
+gs_mock_runway_title <- gs_title("Mock Runway Table")
+gs_MRT <- gs_read(gs_mock_runway_title, ws=1)
 
-Grab_data <- function(variable){
-  
-  gs_mock_runway_title <- gs_title("Mock Runway Table")
-  gs_MRT <- gs_read(gs_mock_runway_title, ws-1)
-}
 
 
