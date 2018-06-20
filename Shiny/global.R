5c72be9ffac8e9f456098528865c09c8fa2a80e6
zacharyobrien <zach@invisible.email>
2018-06-20 11:11:31 -0700

    Remove mock code

--- Shiny/global.R
+++ Shiny/global.R
@@ -3,8 +3,6 @@ source("Server.R")
 source("UI.R")
 source("global.R")
 
-data <- readRDS("healthexp.Rds")
-data$Region <- as.factor(data$Region)
 
 # Run the app ----
 shinyApp(ui = ui, server = server)
