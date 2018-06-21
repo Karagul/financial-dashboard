d9e82267b7dd4f662919326c599b76aa2e5ef522
liamob <liamob@umich.edu>
2018-06-21 08:58:43 -0700

    Added UI to Partner page/ works online.

--- Shiny/app.R
+++ Shiny/app.R
@@ -4,14 +4,16 @@ library(googleVis)
 library(googlesheets)
 library(DT)
 library(shinythemes)
+load('gs_MRT')
+
 
 # Define UI for application that plots features of movies 
 ui <- fluidPage(theme = shinytheme("sandstone"),
                 
  #top bar
-   navbarPage("Invisible",
+   navbarPage("Invisible Technologies",
               tabPanel("Investors",
-                  titlePanel("Invisible Model 3.0", windowTitle = "TheMoodel3.0"), # App title
+                  titlePanel("The Model", windowTitle = "TheMoodel3.0"), # App title
                   sidebarLayout( # Sidebar layout with a input and output definitions
                     sidebarPanel(
                       sliderInput(inputId = "Month",
@@ -19,19 +21,23 @@ ui <- fluidPage(theme = shinytheme("sandstone"),
                                   step = 1,
                                   min = 1,
                                   max = nrow(gs_MRT),
-                                  value = c(3,nrow(gs_MRT) - 5)
+                                  value = c(3,nrow(gs_MRT) - 5),
+                                  animate = TRUE
                                   ),
                       
                       selectInput(inputId = "Growth Rate",
                                   label = "Percentage Growth:",
-                                  choices = c("Exponential: 10%/Month", "Exponential: 20%/Month",
-                                              "Exponential: 25%/Month","Exponential: 40%/Month", 
-                                              "Exponential: 50%/Month","Exponential: 75%/Month",
-                                              "Exponential: 100%/Month","Exponential: 150%/Month")
+                                  choices = c("Flat", "Linear: 5k/mo",
+                                              "Linear: 10k/mo","Linear 20k/mo", 
+                                              "Exponential: 5%/mo","Exponential: 15%/mo",
+                                              "Exponential: 20%/mo","Exponential: 30%/mo")
                                   ),
-                      radioButtons(inputId = "Options",
-                                   choices = c("Color","Year", "Type","Visualization"),
-                                   label = "Other Options")
+                      checkboxGroupInput(inputId = "Options",
+                                         label = "Other Options:",
+                                         choices = c("Inlcude Agent Guarantee of 30 hrs/mo",
+                                                     "Monty Carlo Simulation", 
+                                                     "Inlcude Varience in Projetions"),
+                                        selected = c("Type"))
                     ),
                        # Outputs
                       mainPanel(
@@ -46,7 +52,46 @@ ui <- fluidPage(theme = shinytheme("sandstone"),
                      )
                   )
               ),
-              tabPanel("Partners"),
+              tabPanel("Partners",
+                       titlePanel("The Model 3.0", windowTitle = "TheMoodel3.0"), # App title
+                       sidebarLayout( # Sidebar layout with a input and output definitions
+                         sidebarPanel(
+                           sliderInput(inputId = "Month",
+                                       label = "Number of months:",
+                                       step = 1,
+                                       min = 1,
+                                       max = nrow(gs_MRT),
+                                       value = c(3,nrow(gs_MRT) - 5),
+                                       animate = TRUE
+                           ),
+                           
+                           selectInput(inputId = "Growth Rate",
+                                       label = "Percentage Growth:",
+                                       choices = c("Flat", "Linear: 5k/mo",
+                                                   "Linear: 10k/mo","Linear 20k/mo", 
+                                                   "Exponential: 5%/mo","Exponential: 15%/mo",
+                                                   "Exponential: 20%/mo","Exponential: 30%/mo")
+                           ),
+                           checkboxGroupInput(inputId = "Options",
+                                              label = "Other Options:",
+                                              choices = c("Inlcude Agent Guarantee of 30 hrs/mo",
+                                                          "Monty Carlo Simulation", 
+                                                          "Inlcude Varience in Projetions"),
+                                              selected = c("Type"))
+                         ),
+                         # Outputs
+                         mainPanel(
+                           tabsetPanel(type = "tab",
+                                       tabPanel("Profit", plotOutput(outputId = "Bar_graph3.0")),
+                                       tabPanel("Runway", plotOutput(outputId ="line_graph3.0")),
+                                       tabPanel("Revenue"),
+                                       tabPanel("Labor Cost"),
+                                       tabPanel("Partner Pay"),
+                                       tabPanel("Other")
+                           )
+                         )
+                       )
+              ),
               navbarMenu("More",
                          tabPanel("Spreadsheet",
                                   dataTableOutput(outputId = "data")
@@ -61,11 +106,11 @@ ui <- fluidPage(theme = shinytheme("sandstone"),
 server <- function(input, output) {
   
 
-  
+  ###################################INVESTOR PAGE#############################################
   # Create scatterplot object the plotOutput function is expecting
   output$Bar_graph <- renderPlot({
-    ggplot(data = gs_MRT, aes(x = Month, y = Revenue)) +
-      geom_bar(stat = "identity", colour = "yellow")
+    ggplot(data = gs_MRT, aes(x = Comissions, y = Revenue)) +
+      geom_bar(stat = "identity")
   })
   
   output$line_graph <- renderPlot({
@@ -73,10 +118,24 @@ server <- function(input, output) {
       geom_bar(stat = "identity")
   })
   
+
+  
+######################################PARTNER PAGE################################################
+  output$Bar_graph3.0 <- renderPlot({
+    ggplot(data = gs_MRT, aes(x = Comissions, y = Revenue)) +
+      geom_bar(stat = "identity", color = "yellow")
+  })
+  
+  output$line_graph3.0 <- renderPlot({
+    ggplot(data = gs_MRT, aes(x = Month, y = Comissions)) +
+      geom_bar(stat = "identity", color = "red")
+  })
+  
+#########################################MORE PAGE###############################################
   output$data <- renderDataTable({
     DT::datatable(data = gs_MRT,
-              options = list(pageLength = 20, lengthMenu = c(10, 25, 40)), 
-              rownames = FALSE)
+                  options = list(pageLength = 20, lengthMenu = c(10, 25, 40)), 
+                  rownames = FALSE)
   })
   
 }
