b1974ea8d6aa30d84bef95a2f0cb890738788652
liamob <liamob@umich.edu>
2018-06-22 16:50:32 -0700

    added more sliders

--- Shiny/app.R
+++ Shiny/app.R
@@ -4,7 +4,7 @@ library(googleVis)
 library(googlesheets)
 library(DT)
 library(shinythemes)
-load('gs_MRT2')
+load('gs_MRT3')
 
 
 # Define UI for application that plots features of movies 
@@ -13,33 +13,62 @@ ui <- fluidPage(theme = shinytheme("sandstone"),
    navbarPage("Invisible Technologies",
               tabPanel("Investors",
                   titlePanel("The Model", windowTitle = "TheMoodel3.0"), # App title
-                  sidebarLayout( # Sidebar layout with a input and output definitions
+                  sidebarLayout(position = "right", # Sidebar layout with a input and output definitions
                     sidebarPanel(
                       sliderInput(inputId = "moSlider",
                                   label = "Number of months:",
                                   step = 1,
                                   min = 1,
-                                  max = nrow(gs_MRT2),
+                                  max = nrow(gs_MRT3),
                                   value = 10,
                                   animate = TRUE
                                   ),
-                      hr(),
+                      sliderInput(inputId = "mcost",
+                                  label = "Costs Multiplier:* (UNDER CONSTRUCTION)",
+                                  step = 1,
+                                  min = 1,
+                                  max = 10,
+                                  value = 5,
+                                  animate = TRUE
+                      ),
                       
                       selectInput(inputId = "Growth Rate",
                                   label = "Percentage Growth:",
                                   choices = c("Flat", "Linear: 5k/mo",
-                                              "Linear: 10k/mo","Linear 20k/mo", 
+                                              "Linear: 10k/mo","Linear: 20k/mo", 
                                               "Exponential: 5%/mo","Exponential: 15%/mo",
                                               "Exponential: 20%/mo","Exponential: 30%/mo")
                                   ),
                       hr(),
+                      
+                      
+                      # sliderInput(inputId = "vcost",
+                      #             label = "Variable Costs:",
+                      #             step = 1,
+                      #             min = 1,
+                      #             max = 10,
+                      #             value = 5,
+                      #             animate = TRUE
+                      #             ),
+                      # sliderInput(inputId = "Pbonus",
+                      #             label = "Partner Bonuses:",
+                      #             step = 1,
+                      #             min = 1,
+                      #             max = 10,
+                      #             value = 5,
+                      #             animate = TRUE
+                      # ),
                       checkboxGroupInput(inputId = "Options",
                                          label = "Other Options:",
                                          choices = c("Include Agent Guarantee of 30 hrs/mo",
                                                      "Monty Carlo Simulation", 
                                                      "Include Varience in Projetions"),
-                                        selected = c("Type"))
-                    ),
+                                        selected = c("Type")),
+                    h6("* Cost multiplier refers to the percentage that Business Development Costs, 
+                       Fixed Costs, Research and Development Costs, Labor Costs, 
+                       and Sales/Marketing Costs increase at relative to revenue.")
+                      ),
+                    
                        # Outputs
                       mainPanel(
                          tabsetPanel(type = "tab",
@@ -62,8 +91,8 @@ ui <- fluidPage(theme = shinytheme("sandstone"),
                                        label = "Number of months:",
                                        step = 1,
                                        min = 1,
-                                       max = nrow(gs_MRT2),
-                                       value = c(3,nrow(gs_MRT2) - 5),
+                                       max = nrow(gs_MRT3),
+                                       value = c(3,nrow(gs_MRT3) - 5),
                                        animate = TRUE
                            ),
                            hr(),
@@ -116,9 +145,9 @@ server <- function(input, output) {
   ###################################INVESTOR PAGE#############################################
   #####################################Profit Page##############################
   output$profit <- renderGvis({
-    gross_profit_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT2$`Gross Profit`))
-    net_profit_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT2$`Net Profit`))
-    df=data.frame(Month = gs_MRT2[input$moSlider,1], 
+    gross_profit_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT3$`Gross Profit`))
+    net_profit_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT3$`Net Profit`))
+    df=data.frame(Month = gs_MRT3[input$moSlider,1], 
                   Gross_Profit = gross_profit_numeric[1:input$moSlider],
                   Net_Profits = net_profit_numeric[1:input$moSlider])
     gvisColumnChart(df, xvar = "Month", 
@@ -128,10 +157,10 @@ server <- function(input, output) {
   
   ################################Runway page################################
   output$runway <- renderGvis({
-    cash_in_bank_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT2$`Cash in bank at end of month`))
-    net_profit_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT2$`Net Profit`))
-    funds_raised_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT2$`Fundraising/external cash injection`))
-    df=data.frame(Month= gs_MRT2[input$moSlider,1], 
+    cash_in_bank_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT3$`Cash in Bank`))
+    net_profit_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT3$`Net Profit`))
+    funds_raised_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT3$`Cumulative Fundraising`))
+    df=data.frame(Month= gs_MRT3[input$moSlider,1], 
                   Cash_in_Bank= cash_in_bank_numeric[1:input$moSlider],
                   Net_Profit = net_profit_numeric[1:input$moSlider],
                   Funds_Raised = funds_raised_numeric[1:input$moSlider])
@@ -141,19 +170,19 @@ server <- function(input, output) {
   ############################Labor Cost Page########################
   
   output$Lcost <- renderGvis({
-    Agent_Labor_cost_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT2$`Agent Labor Costs`))
-    operators_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT2$Operators))
-    RRR_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT2$RRRs))
-    sentries_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT2$Sentries))
-    strategists_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT2$Strategists))
-    specialists_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT2$Specialists))
-    df=data.frame(Month = gs_MRT2[,1], 
-                  Agent_Labor_Cost = Agent_Labor_cost_numeric[1:nrow(gs_MRT)],
-                  Operator_Labor_cost = operators_numeric[1:nrow(gs_MRT2)],
-                  RRR_Labor_Cost = RRR_numeric[1:nrow(gs_MRT2)],
-                  Sentry_Labor_Cost = sentries_numeric[1:nrow(gs_MRT)],
-                  Strategists_Labor_Cost = strategists_numeric[1:nrow(gs_MRT)],
-                  Specialists_Labor_Cost = specialists_numeric[1:nrow(gs_MRT)]
+    Agent_Labor_cost_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT3$`Agent Labor Costs`))
+    operators_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT3$Operators))
+    RRR_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT3$RRRs))
+    sentries_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT3$Sentries))
+    strategists_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT3$Strategists))
+    specialists_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT3$Specialists))
+    df=data.frame(Month = gs_MRT3[input$moSlider,1], 
+                  Agent_Labor_Cost = Agent_Labor_cost_numeric[1:input$moSlider],
+                  Operator_Labor_cost = operators_numeric[1:input$moSlider],
+                  RRR_Labor_Cost = RRR_numeric[1:input$moSlider],
+                  Sentry_Labor_Cost = sentries_numeric[1:input$moSlider],
+                  Strategists_Labor_Cost = strategists_numeric[1:input$moSlider],
+                  Specialists_Labor_Cost = specialists_numeric[1:input$moSlider]
                   
     )
     gvisComboChart(df, xvar="Month",
@@ -165,19 +194,19 @@ server <- function(input, output) {
 
   ###########################Revenue Page############################
   output$gchart <-renderGvis({
-    revenue_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT2$Revenue))
-    df=data.frame(month= gs_MRT2[,1], 
-                  Revenue= revenue_numeric[1:nrow(gs_MRT2)])
+    revenue_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT3$Revenue))
+    df=data.frame(month= gs_MRT3[input$moSlider,1], 
+                  Revenue= revenue_numeric[1:input$moSlider])
     gvisBarChart(df, options=list(width = 800, height = 400))
   })
   
   ##########Partner pay/gross revenue##################
   output$combo <- renderGvis({
-    partner_pay_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT2$`Partner Pay`))
-    gross_profits_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT2$`Gross Profit`))
-    df=data.frame(Month = gs_MRT2[,1], 
-                  Partner_Pay = partner_pay_numeric[1:nrow(gs_MRT2)],
-                  Gross_Profits = gross_profits_numeric[1:nrow(gs_MRT2)])
+    partner_pay_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT3$`Partner Pay`))
+    gross_profits_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT3$`Gross Profit`))
+    df=data.frame(Month = gs_MRT3[input$moSlider,1], 
+                  Partner_Pay = partner_pay_numeric[1:input$moSlider],
+                  Gross_Profits = gross_profits_numeric[1:input$moSlider])
     gvisComboChart(df, xvar = "Month", 
                   yvar = c("Partner_Pay", "Gross_Profits"),
                   options=list(seriesType="bars",
@@ -186,23 +215,23 @@ server <- function(input, output) {
   
 #########other####################
   output$gline <- renderGvis({
-    revenue_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT2$Revenue))
-    df=data.frame(month= gs_MRT2[,1], 
-                  Revenue= revenue_numeric[1:nrow(gs_MRT2)])
+    revenue_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT3$Revenue))
+    df=data.frame(month= gs_MRT3[input$moSlider,1], 
+                  Revenue= revenue_numeric[1:input$moSlider])
     gvisScatterChart(df)
   })
 
   
 ######################################PARTNER PAGE################################################
   output$Bar_graph3.0 <- renderPlot({
-    revenue_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT2$Revenue))
-    ggplot(data = gs_MRT2, aes(x = Month, y = revenue_numeric)) +
+    revenue_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT3$Revenue))
+    ggplot(data = gs_MRT3, aes(x = Month, y = revenue_numeric)) +
       geom_bar(stat = "identity", color = "yellow")
   })
   
   output$line_graph3.0 <- renderPlot({
-    Labor_Cost = as.numeric(gsub("[\\$,]", "", gs_MRT2$`Labor Cost`))
-    ggplot(data = gs_MRT2, aes(x = Month, y = Labor_Cost)) +
+    Labor_Cost = as.numeric(gsub("[\\$,]", "", gs_MRT3$`Labor Cost`))
+    ggplot(data = gs_MRT3, aes(x = Month, y = Labor_Cost)) +
       geom_bar(stat = "identity", color = "red")
   })
   
@@ -216,7 +245,7 @@ server <- function(input, output) {
 #  })
   #######################################spreadsheet##############################################
     output$table <-renderGvis({
-      gvisTable(gs_MRT2[,c(1:15, 21)])
+      gvisTable(gs_MRT3[,c(1:14, 22)])
     })
 }
 
