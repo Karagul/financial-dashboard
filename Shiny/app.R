dfb2a7de8d0c7c7897c7446385cc4863e4c357a8
liamob <liamob@umich.edu>
2018-06-22 09:09:39 -0700

    Added google charts to web app

--- Shiny/app.R
+++ Shiny/app.R
@@ -44,12 +44,12 @@ ui <- fluidPage(theme = shinytheme("sandstone"),
                        # Outputs
                       mainPanel(
                          tabsetPanel(type = "tab",
-                                     tabPanel("Profit", plotOutput(outputId = "Bar_graph")),
-                                     tabPanel("Runway", plotOutput(outputId ="line_graph")),
-                                     tabPanel("Revenue"),#htmlOutput("table")),
-                                     tabPanel("Labor Cost", htmlOutput("gchart")),
-                                     tabPanel("Partner Pay", htmlOutput("gline")),
-                                     tabPanel("Other")
+                                     tabPanel("Profit", htmlOutput("profit")),
+                                     tabPanel("Runway", htmlOutput("runway")),
+                                     tabPanel("Revenue", htmlOutput("gchart")),
+                                     tabPanel("Labor Cost", htmlOutput("Lcost")),
+                                     tabPanel("Partner Pay", htmlOutput("combo")),
+                                     tabPanel("Other", htmlOutput("gline"))
                         )
                      )
                   )
@@ -101,7 +101,9 @@ ui <- fluidPage(theme = shinytheme("sandstone"),
                          tabPanel("Spreadsheet",
                                   htmlOutput("table")#dataTableOutput(outputId = "data")
                          ),
-                         tabPanel("About")
+                         tabPanel("demo",
+                                  mainPanel(uiOutput("video"))
+                                  )
               )
      )
   )
@@ -112,35 +114,85 @@ server <- function(input, output) {
   
 
   ###################################INVESTOR PAGE#############################################
-  # Create scatterplot object the plotOutput function is expecting
-  output$Bar_graph <- renderPlot({
-    ggplot(data = gs_MRT, aes(x = Month, y = Revenue)) +
-      geom_bar(stat = "identity")
+  #####################################Profit Page##############################
+  output$profit <- renderGvis({
+    gross_profit_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT$`Gross Profit`))
+    net_profit_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT$`Net Profit`))
+    df=data.frame(Month = gs_MRT[,1], 
+                  Gross_Profit = gross_profit_numeric[1:nrow(gs_MRT)],
+                  Net_Profits = net_profit_numeric[1:nrow(gs_MRT)])
+    gvisSteppedAreaChart(df, xvar = "Month", 
+                   yvar = c("Gross_Profit", "Net_Profits"),
+                   options=list(isStaked = TRUE, width = 800, height = 400))
   })
   
-  output$line_graph <- renderPlot({
-    ggplot(data = gs_MRT, aes(x = Month, y = Comissions)) +
-      geom_bar(stat = "identity")
+  ################################Runway page################################
+  output$runway <- renderGvis({
+    cash_in_bank_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT$`Cash in bank at end of month`))
+    net_profit_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT$`Net Profit`))
+    funds_raised_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT$`Fundraising/external cash injection`))
+    df=data.frame(Month= gs_MRT[,1], 
+                  Cash_in_Bank= cash_in_bank_numeric[1:nrow(gs_MRT)],
+                  Net_Profit = net_profit_numeric[1:nrow(gs_MRT)],
+                  Funds_Raised = funds_raised_numeric[1:nrow(gs_MRT)])
+    gvisLineChart(df, options=list(width = 800, height = 400))
+  })
+  
+  ############################Labor Cost Page########################
+  
+  output$Lcost <- renderGvis({
+    operators_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT$Operators))
+    RRR_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT$RRRs))
+    df=data.frame(Month = gs_MRT[,1], 
+                  Operator_Labor_cost = operators_numeric[1:nrow(gs_MRT)],
+                  RRR_Labor_Cost = RRR_numeric[1:nrow(gs_MRT)]
+    )
+    gvisComboChart(df, xvar="Month",
+                            yvar=c("Operator_Labor_cost", "RRR_Labor_Cost"),
+                            options=list(seriesType="bars",
+                                         series='{1: {type:"line"}}'))
   })
 
-
+  ###########################Revenue Page############################
   output$gchart <-renderGvis({
-    gvisBarChart(gs_MRT)
+    revenue_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT$Revenue))
+    df=data.frame(month= gs_MRT[,1], 
+                  Revenue= revenue_numeric[1:nrow(gs_MRT)])
+    gvisBarChart(df, options=list(width = 800, height = 400))
   })
-
+  
+  ##########Partner pay/gross revenue##################
+  output$combo <- renderGvis({
+    partner_pay_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT$`Partner Pay`))
+    gross_profits_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT$`Gross Profit`))
+    df=data.frame(Month = gs_MRT[,1], 
+                  Partner_Pay = partner_pay_numeric[1:nrow(gs_MRT)],
+                  Gross_Profits = gross_profits_numeric[1:nrow(gs_MRT)])
+    gvisComboChart(df, xvar = "Month", 
+                  yvar = c("Partner_Pay", "Gross_Profits"),
+                  options=list(seriesType="bars",
+                               series='{1: {type:"line"}}', width = 800, height = 400))
+  })
+  
+#########other####################
   output$gline <- renderGvis({
-    gvisLineChart(gs_MRT)
+    revenue_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT$Revenue))
+    df=data.frame(month= gs_MRT[,1], 
+                  Revenue= revenue_numeric[1:nrow(gs_MRT)])
+    gvisLineChart(df)
   })
 
   
 ######################################PARTNER PAGE################################################
   output$Bar_graph3.0 <- renderPlot({
-    ggplot(data = gs_MRT, aes(x = Comissions, y = Revenue)) +
+    revenue_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT$Revenue))
+    ggplot(data = gs_MRT, aes(x = Month, y = revenue_numeric)) +
       geom_bar(stat = "identity", color = "yellow")
   })
   
   output$line_graph3.0 <- renderPlot({
-    ggplot(data = gs_MRT, aes(x = Month, y = Comissions)) +
+    Labor_Cost = as.numeric(gsub("[\\$,]", "", gs_MRT$`Labor Cost`))
+    ggplot(data = gs_MRT, aes(x = Month, y = Labor_Cost)) +
       geom_bar(stat = "identity", color = "red")
   })
   
@@ -155,6 +207,11 @@ server <- function(input, output) {
     output$table <-renderGvis({
       gvisTable(gs_MRT)
     })
+  
+  #####################demo##########################
+  output$video <- renderUI({
+    tags$video(src = "https://www.youtube.com/watch?v=tX1gtG--OGE", type = "video/mp4", autoplay = NA, controls = NA)
+  })
   
 }
 
--- Shiny/sheets_data.R
+++ Shiny/sheets_data.R
@@ -4,5 +4,21 @@ gs_mock_runway_title <- gs_title("Mock Runway Table")
 gs_MRT <- gs_read(gs_mock_runway_title, ws=1)
 
 
+operators_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT$Operators))
+RRR_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT$RRRs))
+sentries_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT$Sentries))
+strategists_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT$Strategists))
+specialists_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT$Specialists))
+Agent_Labor_cost_numeric = as.numeric(gsub("[\\$,]", "", gs_MRT$`Agent Labor Costs`))
+
+df=data.frame(Month = gs_MRT[,1], 
+              Operator_Labor_cost = operators_numeric[1:nrow(gs_MRT)],
+              #RRR_Labor_Cost = RRR_numeric[1:nrow(gs_MRT)],
+              Sentry_Labor_Cost = sentries_numeric[1:nrow(gs_MRT)])#,
+#Strategists_Labor_Cost = strategists_numeric[1:nrow(gs_MRT)],
+#Specialists_Labor_Cost = specialists_numeric[1:nrow(gs_MRT)],
+#Agent_Labor_Cost = Agent_Labor_cost_numeric[1:nrow(gs_MRT)])
+
+
 
 
